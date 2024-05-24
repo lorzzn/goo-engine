fa7ff3aabdc265bae9495569f14b58592787c2aa
lorenzozane <lorzzn@qq.com>
2024-05-25 01:40:00 +0800

    update

---
+++ .github/scripts/build.py
@@ -0,0 +1,26 @@
+import sys
+import os
+from os import path
+import subprocess
+
+print("Checkout submodules")
+subprocess.check_call(['git','submodule','update','--init','--recursive'])
+
+BUILD_DIR = path.join('..','build_release')
+
+print("Make sure the build directory exists")
+if path.exists(BUILD_DIR) == False:
+    os.mkdir(BUILD_DIR)
+
+print("Pre-build")
+if sys.platform.startswith('linux'):
+    subprocess.check_call(['python','./build_files/build_environment/install_linux_packages.py'])
+    subprocess.check_call(['python','./build_files/utils/make_update.py --use-linux-libraries'])
+
+if sys.platform.startswith('darwin'):
+    subprocess.check_call(['xcode-select','--install'])
+    subprocess.check_call(['brew','install','cmake git git-lfs svn'])
+
+print("Build Blender project")
+subprocess.check_call(['make','update'])
+subprocess.check_call(['make'])
